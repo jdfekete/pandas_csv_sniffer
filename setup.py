581fb923b24101309c5f36993410f4e1cdc34f6d
FEKETE Jean-Daniel <jean-daniel.fekete@inria.fr>
2021-05-11 15:30:01 +0200

    Adopted new python project layout

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
-#!/usr/bin/env python
+import setuptools
 
-from setuptools import setup
-
-setup(
-    name='pandas_csv_sniffer',
-    version='0.1',
-    description='Jupyter notebook GUI to pandas.csv_read',
-    author='Jean-Daniel Fekete',
-    author_email='Jean-Daniel.Fekete@inria.fr',
-    url='https://github.com/jdfekete/pandas_csv_sniffer',
-    license="MIT",
-    packages=['csv_sniffer'],
-    platforms='any',
-    setup_requires=['nose'],
-    test_suite='nose.collector',
-    install_requires=[
-        "pandas",
-        "fsspec",
-        "ipywidgets"
-    ]
-)
+setuptools.setup()
